670dc8837e0ed9c9e3112db38dd9d6b894ee5ace
droque21 <davidroque21@hotmail.com>
2022-05-03 16:10:31 -0400

    add error user not found when finding by username

--- src/application/use-cases/user/getUserByUsername.ts
+++ src/application/use-cases/user/getUserByUsername.ts
@@ -1,7 +1,9 @@
 import { User } from "../../../domain/entities/user";
+import { generateCustomError } from "../../../infrastructure/webserver/middlewares/errorHandlingMiddleware";
 import { UserRepository } from "../../interfaces/user.repository.interface";
 
 export const getUserByUsername = async (username: string, userRepository: UserRepository): Promise<User> => {
   const result = await userRepository.getUserByUsername(username);
+  if (!result) generateCustomError(404, "User not found")
   return result;
 }
